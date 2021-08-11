e5c42e951b51555e8351a1b7b608449a1e64ba8d
“nombre” <email>
2021-08-11 16:39:08 +0200

    Importamos en app.module.ts el modulo FormsModule para usar ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CorreoComponent } from './Components/correo/correo.component';
+import { ListaCorreosComponent } from './Components/lista-correos/lista-correos.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CorreoComponent,
+    ListaCorreosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
