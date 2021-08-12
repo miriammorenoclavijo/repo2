228438aab354ca3c46f720b8179a0bf861a1fdfb
“nombre” <email>
2021-08-12 13:21:09 +0200

    incluimos el componente y el modulo para el formulario reactivo

---
+++ src/app/Components/nuevo-correo/nuevo-correo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nuevo-correo',
+  templateUrl: './nuevo-correo.component.html',
+  styleUrls: ['./nuevo-correo.component.css']
+})
+export class NuevoCorreoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NuevoCorreoComponent } from './Components/nuevo-correo/nuevo-correo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NuevoCorreoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
