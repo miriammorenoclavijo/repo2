bb32a47f6dadac6d11101ffea9e7836692fe0e95
“nombre” <email>
2021-08-12 13:40:49 +0200

    incluimos el formulario reactivo

--- src/app/Components/nuevo-correo/nuevo-correo.component.ts
+++ src/app/Components/nuevo-correo/nuevo-correo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-nuevo-correo',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NuevoCorreoComponent implements OnInit {
 
-  constructor() { }
+  nuevoCorreo!: FormGroup;
+  submitted = false;
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) { 
+    
   }
+
+  ngOnInit() {
+    this.nuevoCorreo = this.formBuilder.group({
+      titulo: ['Incluye un título', [Validators.required, Validators.minLength(3)]],
+      cuerpo: ['', [Validators.required, Validators.minLength(10)]],
+      destinatario: ['', [Validators.required, Validators.email]],
+    });
+}
+
+get formulario() { return this.nuevoCorreo.controls; }
+
+onSubmit() {
+    this.submitted = true;
+
+    if (this.nuevoCorreo.invalid) {
+        return;
+    }
+
+    let correo = this.nuevoCorreo.value; //asigna el objeto completo creado en OnInit 
+    correo.leido= false;
+    correo.emisor= 'correoEmisor1@openWebinar.inv';
+
+    alert("Correo Enviado \nEliminamos el formulario");
+    this.onReset();
+}
+
+onReset() {
+    this.submitted = false;
+    this.nuevoCorreo.reset();
+}
 
 }
