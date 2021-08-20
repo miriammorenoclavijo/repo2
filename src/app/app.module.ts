f2d89e52c8b29c1d6044d5852e7a232184be8196
“nombre” <email>
2021-08-20 12:09:55 +0200

    Creamos con ng un componente de avisos, un servicio y una nueva vista… … para correos recibidos, donde vamos a integrarlo todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NuevoCorreoComponent } from './Components/nuevo-correo/nuevo-correo.component';
 import { ListaCorreosComponent } from './Components/lista-correos/lista-correos.component';
+import { AvisosComponent } from './Components/avisos/avisos.component';
+import { CorreosRecibidosComponent } from './Views/correos-recibidos/correos-recibidos/correos-recibidos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NuevoCorreoComponent,
-    ListaCorreosComponent
+    ListaCorreosComponent,
+    AvisosComponent,
+    CorreosRecibidosComponent
   ],
   imports: [
     BrowserModule,
