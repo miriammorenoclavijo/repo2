b4add26a91663a86c42075347fef23dfb4d44d11
“nombre” <email>
2021-08-20 14:59:32 +0200

    Integramos el componente login en vista de correos recibidos y config… …uramos en app.module el modulo ng-gapi para su uso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,27 @@ import { AvisosComponent } from './Components/avisos/avisos.component';
 import { CorreosRecibidosComponent } from './Views/correos-recibidos/correos-recibidos/correos-recibidos.component';
 import { LoginComponent } from './login/login.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import {
+  GoogleApiModule,
+  NgGapiClientConfig,
+  NG_GAPI_CONFIG,
+} from "ng-gapi";
+
+let gapiClientConfig: NgGapiClientConfig = {
+  client_id: "711026066989-etpfdmdqtpm2bruf0nngagkkniht0jlr.apps.googleusercontent.com",
+  discoveryDocs: ["https://analyticsreporting.googleapis.com/$discovery/rest?version=v4"],
+  ux_mode: "popup",
+  redirect_uri: "http://localhost:4200/loged",
+  scope: [
+    "https://www.googleapis.com/auth/userinfo.profile",
+    "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/gmail.labels",
+    "https://www.googleapis.com/auth/gmail.send",
+    "https://www.googleapis.com/auth/gmail.readonly"
+  ].join(" ")
+};
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +43,12 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    GoogleApiModule.forRoot({
+      provide: NG_GAPI_CONFIG,
+      useValue: gapiClientConfig
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
